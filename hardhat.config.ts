24b0aeb096aea795faa380c3cdd77495149c1c4f
pat <dimuziop@gmail.com>
2022-08-08 10:39:05 +0200

    last config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -4,10 +4,6 @@ import "@nomicfoundation/hardhat-toolbox";
 const config: HardhatUserConfig = {
   solidity: "0.8.9",
   networks: {
-    rinkeby_test: {
-      url: `https://rinkeby.infura.io/v3/ab940c95e51c4eea9e3dc5d108ddfffc`,
-      accounts: [`0x` + "your_private_key"]
-    },
     polygon: {
       url: `https://polygon-mainnet.infura.io/v3/ab940c95e51c4eea9e3dc5d108ddfffc`,
       accounts: [`0x` + "your_private_key"]
